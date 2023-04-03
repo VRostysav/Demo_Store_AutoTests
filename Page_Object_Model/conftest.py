c5642a037e2dc506bde826fa94fae7135e6cb909
Family <v.rostyk2001@gmail.com>
2023-04-03 10:17:33 +0300

    Headless mode for FF and Chrome

--- Page_Object_Model/conftest.py
+++ Page_Object_Model/conftest.py
@@ -1,12 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChOptions
+from selenium.webdriver.firefox.options import Options as FFOptions
 import os
 
 
 @pytest.fixture(scope="class")
 def init_driver(request):
-    supported_browsers = ['chrome', 'ch', 'firefox', 'ff', 'headlesschrome']
-    os.environ['BROWSER'] = 'ch'
+    supported_browsers = ['chrome', 'ch', 'firefox', 'ff', 'headlesschrome', 'headlessfirefox']
+    os.environ['BROWSER'] = 'headlesschrome'
     browser = os.environ.get('BROWSER', None)
 
     if not browser:
@@ -20,7 +22,19 @@ def init_driver(request):
         driver = webdriver.Chrome()
     elif browser in ('ff', 'firefox'):
         driver = webdriver.Firefox()
+    elif browser in ('headlesschrome'):
+        chrome_options = ChOptions()
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(options=chrome_options)
+    elif browser in ('headlessfirefox'):
+        ff_options = FFOptions()
+        ff_options.add_argument('--disable-gpu')
+        ff_options.add_argument('--no-sandbox')
+        ff_options.add_argument('--headless')
+        driver = webdriver.Firefox(options=ff_options)
 
     request.cls.driver = driver
     yield
-    #driver.quit()
+    # driver.quit()
--- Page_Object_Model/tests/my_account/test_login_negative.py
+++ Page_Object_Model/tests/my_account/test_login_negative.py
@@ -8,8 +8,8 @@ class TestLoginNegative:
     def test_login_none_existing_user(self):
         my_account = MyAccountSignedOut(self.driver)
         my_account.go_to_my_account()
-        my_account.input_login_username('dadasda')
-        my_account.input_login_password('sadadasSDA21')
+        # my_account.input_login_username('dadasda')
+        # my_account.input_login_password('sadadasSDA21')
         my_account.click_log_in_button()
         error_text = 'Error: Username is required.'
         my_account.verify_error_message(error_text)
